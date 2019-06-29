3780b06cc019ab1bfe38580bc39f24b8b501f2ea
pyystone <pyystone@163.com>
2019-06-30 03:19:38 +0800

    上传打包文件

--- exam/Properties/AssemblyInfo.cs
+++ exam/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("exam")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("简易做题系统")]
+[assembly: AssemblyDescription("就是一个非常非常简单的做题系统")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("exam")]
+[assembly: AssemblyCompany("pyystone")]
+[assembly: AssemblyProduct("简易做题系统")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
